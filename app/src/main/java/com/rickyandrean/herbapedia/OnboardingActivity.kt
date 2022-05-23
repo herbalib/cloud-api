27cd3769b5965247a7356e6a63f42134ea91cea3
rickyandrean99 <ricky.ralph09@gmail.com>
2022-05-24 01:05:37 +0700

    Project preparation and onboarding design
    - Preparing resources such as background, font, color, theme and certain image
    - Design onboarding page (exclude animation)

---
+++ app/src/main/java/com/rickyandrean/herbapedia/OnboardingActivity.kt
@@ -0,0 +1,11 @@
+package com.rickyandrean.herbapedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
