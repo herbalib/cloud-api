0035b3f5843c2da3b719ec19cf383950acef1e82
rickyandrean99 <ricky.ralph09@gmail.com>
2022-05-24 04:59:01 +0700

    Create onboarding animation
    Build onboarding animation which consisting of 3 box that the user can swipe it

--- app/src/main/java/com/rickyandrean/herbapedia/OnboardingActivity.kt
+++ app/src/main/java/com/rickyandrean/herbapedia/OnboardingActivity.kt
@@ -1,11 +1,34 @@
 package com.rickyandrean.herbapedia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        supportActionBar?.hide()
     }
 }
