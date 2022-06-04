5ed6e433476dd45a4efe2b348a1b353eea0c1144
rickyandrean99 <ricky.ralph09@gmail.com>
2022-06-04 23:06:44 +0700

    Update detail page
    Create bottom sheet for detail page and update gitignore

--- Android Application/app/src/main/java/com/rickyandrean/herbapedia/ui/detail/DetailActivity.kt
+++ Android Application/app/src/main/java/com/rickyandrean/herbapedia/ui/detail/DetailActivity.kt
@@ -1,7 +1,13 @@
 package com.rickyandrean.herbapedia.ui.detail
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -15,20 +21,51 @@ import com.rickyandrean.herbapedia.model.Plant
 class DetailActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var plant: Plant
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun showData() {
+        binding.bottomSheetPlant.tvBsPlantName.text = plant.name
+        Glide.with(this)
+            .load("https://wallpaper.dog/large/5526740.jpg")
+            .transform(CenterInside(), RoundedCorners(24))
+            .into(binding.bottomSheetPlant.ivBsPlantImage)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val plant = intent.getParcelableExtra<Plant>(PLANT) as Plant
-
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_detail) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        plant = intent.getParcelableExtra<Plant>(PLANT) as Plant
+
+        setupView()
+        showData()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        with(mMap.uiSettings) {
+            isZoomControlsEnabled = true
+            isCompassEnabled = true
+            isMapToolbarEnabled = true
+        }
 
 //        val sydney = LatLng(-34.0, 151.0)
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
