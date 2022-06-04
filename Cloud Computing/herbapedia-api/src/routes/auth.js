d2fe5f1c8ee185b2c54fd7a49471d8a57e0371cb
theophilhenry <s160419078@student.ubaya.ac.id>
2022-06-05 04:31:11 +0700

    Tensor worked, now can receive multipart image

--- Cloud Computing/herbapedia-api/src/routes/auth.js
+++ Cloud Computing/herbapedia-api/src/routes/auth.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const upload = multer()
+const tf = require('@tensorflow/tfjs-node');
+var Jimp = require('jimp');
 
-const { register, login } = require("../controllers/auth");
+const {
+    register,
+    login
+} = require("../controllers/auth");
 
 // Authentication
 router.post("/login", login);
 router.post("/register", register);
-
+router.post("/req-tester", upload.single('image'), (req, res) => {
+    Jimp.read(req.file.buffer)
+        .then(image => {
+            result = tf.tensor(image.bitmap.data).reshape([image.bitmap.height, image.bitmap.width, -1]);
+            // tf.print(result)
+            console.log(result.)
+        })
+        .catch(err => {
+            res.send(err)
+        });
+})
 
 module.exports = router;
