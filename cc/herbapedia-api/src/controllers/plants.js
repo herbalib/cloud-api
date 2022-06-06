6eea48bc38426021ec3185aceab67cc766ce2b35
theophilhenry <s160419078@student.ubaya.ac.id>
2022-06-07 06:02:10 +0700

    Update plants.js

--- cc/herbapedia-api/src/controllers/plants.js
+++ cc/herbapedia-api/src/controllers/plants.js
@@ -103,7 +103,7 @@ const predict = async (req, res) => {
       // For Prediction
       tensor_3d_original = tf.tensor(image.resize(384, 384).bitmap.data).reshape([image.bitmap.height, image.bitmap.width, -1]).transpose().slice([0, 0, 0], [3, image.bitmap.width, image.bitmap.height]).transpose().reshape([image.bitmap.height, image.bitmap.width, -1]);
       console.log('Input Shape ' + tensor_3d_original.shape)
-      console.log('Data ' + tf.print(tensor_3d_original.shape))
+      tf.print(tensor_3d_original)
 
       try {
         // Import the Model (Through file or local API)
